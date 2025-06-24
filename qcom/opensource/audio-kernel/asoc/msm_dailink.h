5cb9442b838a48118180684aef967a44d4cbc6f0
toufu <hamm157famm7423@gmail.com>
2025-06-24 13:25:12 +0900

    audio-kernel: Add sheng support

--- qcom/opensource/audio-kernel/asoc/msm_dailink.h
+++ qcom/opensource/audio-kernel/asoc/msm_dailink.h
@@ -306,6 +306,16 @@ SND_SOC_DAILINK_DEFS(sec_tdm_rx_0,
 	DAILINK_COMP_ARRAY(COMP_CODEC("cs35l41.2-0040", "cs35l41.2-0040"),
 			    COMP_CODEC("cs35l41.2-0042", "cs35l41.2-0042")),
 	DAILINK_COMP_ARRAY(COMP_PLATFORM("snd-soc-dummy")));
+#elif defined(CONFIG_TARGET_PRODUCT_SHENG)
+SND_SOC_DAILINK_DEFS(sec_tdm_rx_0,
+	DAILINK_COMP_ARRAY(COMP_CPU("snd-soc-dummy-dai")),
+	DAILINK_COMP_ARRAY(COMP_CODEC("cs35l43.3-0040", "cs35l43-pcm"),
+			   		   COMP_CODEC("cs35l43.3-0041", "cs35l43-pcm"),
+					   COMP_CODEC("cs35l43.3-0042", "cs35l43-pcm"),
+					   COMP_CODEC("cs35l43.2-0040", "cs35l43-pcm"),
+					   COMP_CODEC("cs35l43.2-0041", "cs35l43-pcm"),
+					   COMP_CODEC("cs35l43.2-0042", "cs35l43-pcm")),
+	DAILINK_COMP_ARRAY(COMP_PLATFORM("snd-soc-dummy")));
 #else
 SND_SOC_DAILINK_DEFS(sec_tdm_rx_0,
 	DAILINK_COMP_ARRAY(COMP_CPU("snd-soc-dummy-dai")),
